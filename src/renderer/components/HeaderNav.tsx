b24503dfedfa465574a1c586b4c2348be340e6cd
Michael Bouwer <mbouwer3@gmail.com>
2023-01-24 22:57:24 +0200

    Tweaks.

--- src/renderer/components/HeaderNav.tsx
+++ src/renderer/components/HeaderNav.tsx
@@ -1,28 +1,30 @@
 import './HeaderNav.scss';
 
-import { Button, IconButton } from '@mui/material';
+import { Button, IconButton, Typography } from '@mui/material';
 
 import { Link } from 'react-router-dom';
 import { Settings } from '@mui/icons-material';
+import { useState } from 'react';
 
 type Props = {};
 
 const HeaderNav = (props: Props) => {
+  const [page, setPage] = useState('Settings');
   return (
     <div className="header-nav">
-      <section>
-        <p className="title">HeaderNav</p>
+      <section style={{ alignSelf: 'center' }}>
+        <Typography variant="h1">{page}</Typography>
       </section>
       <section>
         <Link to="/timetable">
-          <Button>Timetable</Button>
+          <Button onClick={() => setPage('Timetable')}>Timetable</Button>
         </Link>
         <Link to="/sub-sched">
-          <Button>Free Periods</Button>
+          <Button onClick={() => setPage('Free Periods')}>Free Periods</Button>
         </Link>
         <Link to="/">
-          <IconButton>
-            <Settings  color='warning'/>
+          <IconButton onClick={() => setPage('Settings')}>
+            <Settings color="warning" />
           </IconButton>
         </Link>
       </section>
